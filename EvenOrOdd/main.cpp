69691540677e7dea79e47030175a74dceaf6f51f
Ghostsquadron38 <dokhanhsang159753@gmail.com>
2020-11-26 21:27:58 +0700

    first commit

---
+++ EvenOrOdd/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Input an number: "; cin>>n;
+    if (n%2==0)
+        cout<<n<<" is an even integer"<<endl;
+    else cout<<n<<" is an odd integer"<<endl;
+    return 0;
+}
